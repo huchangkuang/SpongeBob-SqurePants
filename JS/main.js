c3e31144f4572c5c65566b74db5b898ff49739c9
huchangkuang <603021137@qq.com>
2020-06-08 23:50:04 +0800

    完成了暂停、播放、播放速度按钮

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,45 @@
 import string from "./css.js"
+
 const text = document.querySelector("#text")
 const style = document.querySelector("#style")
+const pause = document.querySelector("#btnPause")
+const play = document.querySelector("#btnPlay")
+const slow = document.querySelector("#btnSlow")
+const normal = document.querySelector("#btnNormal")
+const fast = document.querySelector("#btnFast")
+
 let n = 1
-let id = setInterval(() => {
+let time = 20
+let run = ()=>{
     n += 1
     if (n > string.length) {
         window.clearInterval(id)
         return
     }
-    console.log(n)
     text.innerText =  string.substr(0,n)
     style.innerHTML = string.substr(0,n)
     text.scrollTop = text.scrollHeight
-}, 0)
+}
+let id = setInterval(run, time)
+
+pause.onclick = ()=>{
+    window.clearInterval(id)
+}
+play.onclick = ()=>{
+    id = setInterval(run, time)
+}
+slow.onclick = ()=>{
+    window.clearInterval(id)
+    time = 200
+    id = setInterval(run, time)
+}
+normal.onclick = ()=>{
+    window.clearInterval(id)
+    time = 20
+    id = setInterval(run, time)
+}
+fast.onclick = ()=>{
+    window.clearInterval(id)
+    time = 0
+    id = setInterval(run, time)
+}
